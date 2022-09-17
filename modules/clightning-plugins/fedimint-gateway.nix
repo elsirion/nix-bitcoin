3ac1e0e327da4417b71050a78094ef84abd1d300
elsirion <elsirion@protonmail.com>
2022-09-17 15:51:53 +0200

    Add fedimint lightning gateway cln plugin

---
+++ modules/clightning-plugins/fedimint-gateway.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.services.clightning.plugins.fedimint-gw; in
+{
+  options.services.clightning.plugins.fedimint-gw = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable the fedimint lightning gateway (clightning plugin).
+      '';
+    };
+    dataDir = mkOption {
+      type = types.str;
+      default = "${config.services.clightning.dataDir}/fedimint-gw";
+      description = ''
+        Directory that will contain the config and database for the gateway plugin.
+      '';
+    };
+    package = mkOption {
+      type = types.package;
+      default = config.nix-bitcoin.pkgs.fedimint;
+      defaultText = "config.nix-bitcoin.pkgs.fedimint";
+      description = "The package providing the lightning gateway binaries.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.clightning.extraConfig = ''
+      plugin=${cfg.package}/bin/ln_gateway
+      fedimint-cfg=${cfg.dataDir}
+    '';
+  };
+}
